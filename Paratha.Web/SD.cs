aa9aad3a4db70e27820952244769d7b42980f328
Uzair Aslam <uzairaslam64@gmail.com>
2021-08-02 20:00:00 -0600

    Product Repository Interface and Dtos

---
+++ Paratha.Web/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paratha.Web
+{
+    public static class SD
+    {
+        public static string ProductAPIBase { get; set; }
+        public enum APIType { GET, POST, PUT, DELETE }
+    }
+}
---
+++ Paratha.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Paratha.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paratha.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(int id);
+
+    }
+}
