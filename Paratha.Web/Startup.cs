b9a96c208205f6477f87f4ec9b5f15e7a67e6f11
Uzair Aslam <uzairaslam64@gmail.com>
2021-08-05 20:00:00 -0600

    Configure ProductService and HttpClient in Startup

--- Paratha.Web/Startup.cs
+++ Paratha.Web/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Paratha.Web.Services;
+using Paratha.Web.Services.IServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,10 @@ namespace Paratha.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<IProductService, ProductService>();
+            SD.ProductAPIBase = Configuration["ServiceUrls:ProductAPI"];
+            services.AddScoped<IProductService, ProductService>();
+
             services.AddControllersWithViews();
         }
 
